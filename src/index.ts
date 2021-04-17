ef686deea84505597f4bf5f7cdcf0bcdbd1731df
TheNightmareX <TheNightmareX@foxmail.com>
2021-04-17 11:28:29 +0800

    feat: add missing interface exports

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from "./controllers/rest-controller.interface";
 export * from "./controllers/route-names.types";
 export * from "./entity-not-found-error.filter";
 export * from "./services/lookup-fields.type";
+export * from "./services/rest-service-options.interface";
 export * from "./services/rest-service.factory";
 export * from "./services/rest-service.interface";

f277665616d287cbf66f9baed820a1ff6d0288a3
TheNightmareX <TheNightmareX@foxmail.com>
2021-04-17 18:14:11 +0800

    refactor(controller): use interface param types directly

--- src/controllers/rest-controller.factory.ts
+++ src/controllers/rest-controller.factory.ts
@@ -63,57 +63,32 @@ export class RestControllerFactory<
     let restService: Service;
 
     type Interface = RestController<Entity, CreateDto, UpdateDto, LookupField>;
-    /**
-     * ### Metadata in Overriding
-     *
-     * - Controller-level decorators store metadata in the controller, there is **NO
-     * NEED** to decorate controllers again.
-     * - Route-level decorators store metadata in the route method directly, which
-     * will be lost when overriding the routes, so the routes **SHOULD BE** decoratored
-     * again to make them work.
-     * - Param-level decorators store metadata in the controller, so there's **NO
-     * NEED** to decorate the params again, just ensure not to change the order of
-     * the params.
-     *
-     * ### Rest Parameters
-     *
-     * Rest params in each route are for better extendability, without them there
-     * can be no extra params when overloading the route.
-     */
     this.controller = class RestController implements Interface {
       constructor(service: Service) {
         restService = service;
       }
 
-      async list(...args: any[]) {
+      async list(...[]: Parameters<Interface["list"]>) {
         return serialize(await restService.list());
       }
 
-      async create(dto: CreateDto, ...args: any[]) {
+      async create(...[dto]: Parameters<Interface["create"]>) {
         return serialize(await restService.create(dto));
       }
 
-      async retrieve(lookup: Entity[LookupField], ...args: any[]) {
+      async retrieve(...[lookup]: Parameters<Interface["retrieve"]>) {
         return serialize(await restService.retrieve(lookup));
       }
 
-      async replace(
-        lookup: Entity[LookupField],
-        dto: CreateDto,
-        ...args: any[]
-      ) {
+      async replace(...[lookup, dto]: Parameters<Interface["replace"]>) {
         return serialize(await restService.replace(lookup, dto));
       }
 
-      async update(
-        lookup: Entity[LookupField],
-        dto: UpdateDto,
-        ...args: any[]
-      ) {
+      async update(...[lookup, dto]: Parameters<Interface["update"]>) {
         return serialize(await restService.update(lookup, dto));
       }
 
-      async destroy(lookup: Entity[LookupField], ...args: any[]) {
+      async destroy(...[lookup]: Parameters<Interface["destroy"]>) {
         await restService.destroy(lookup);
       }
     };
--- src/controllers/rest-controller.interface.ts
+++ src/controllers/rest-controller.interface.ts
@@ -1,15 +1,45 @@
 import { LookupFields } from "src/services/lookup-fields.type";
 
+/**
+ * ### Metadata in Overriding
+ *
+ * - Controller-level decorators store metadata in the controller, there is **NO
+ * NEED** to decorate controllers again.
+ * - Route-level decorators store metadata in the route method directly, which
+ * will be lost when overriding the routes, so the routes **SHOULD BE** decoratored
+ * again to make them work.
+ * - Param-level decorators store metadata in the controller, so there's **NO
+ * NEED** to decorate the params again, just ensure not to change the order of
+ * the params.
+ *
+ * ### Rest Parameters
+ *
+ * Rest params in each route are for better extendability, without them there
+ * can be no extra params when overloading the route.
+ */
 export interface RestController<
   Entity = any,
   CreateDto = Entity,
   UpdateDto = CreateDto,
   LookupField extends LookupFields<Entity> = LookupFields<Entity>
 > {
-  list(): Promise<Entity[]>;
-  create(dto: CreateDto): Promise<Entity>;
-  retrieve(lookup: Entity[LookupField]): Promise<Entity>;
-  replace(lookup: Entity[LookupField], dto: CreateDto): Promise<Entity>;
-  update(lookup: Entity[LookupField], dto: UpdateDto): Promise<Entity>;
-  destroy(lookup: Entity[LookupField]): Promise<void>;
+  list(...args: any[]): Promise<Entity[]>;
+
+  create(dto: CreateDto, ...args: any[]): Promise<Entity>;
+
+  retrieve(lookup: Entity[LookupField], ...args: any[]): Promise<Entity>;
+
+  replace(
+    lookup: Entity[LookupField],
+    dto: CreateDto,
+    ...args: any[]
+  ): Promise<Entity>;
+
+  update(
+    lookup: Entity[LookupField],
+    dto: UpdateDto,
+    ...args: any[]
+  ): Promise<Entity>;
+
+  destroy(lookup: Entity[LookupField], ...args: any[]): Promise<void>;
 }

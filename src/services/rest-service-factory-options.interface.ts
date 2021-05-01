ed5f0be29281200fd1cd646434b1a0b3b7c5fa68
TheNightmareX <TheNightmareX@foxmail.com>
2021-05-01 23:47:43 +0800

    style(service): add comment to the options interface

--- src/services/rest-service-factory-options.interface.ts
+++ src/services/rest-service-factory-options.interface.ts
@@ -7,11 +7,24 @@ export interface RestServiceFactoryOptions<
   UpdateDto = CreateDto,
   LookupField extends LookupFields<Entity> = LookupFields<Entity>
 > {
+  /**
+   * The entity's constructor whose repository will be auto-injected.
+   */
   entityClass: ClassConstructor<Entity>;
+  /**
+   * Specify the connection name of the entity repository.
+   */
   repoConnection?: string;
+  /**
+   * Be used to infer the generic types and apply validation in the
+   * controller.
+   */
   dtoClasses: {
     create: ClassConstructor<CreateDto>;
     update: ClassConstructor<UpdateDto>;
   };
+  /**
+   * Choose the field used for entity lookup.
+   */
   lookupField: LookupField;
 }

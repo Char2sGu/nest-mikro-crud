8f7ce7f68189e7270c55f365c17d415fcc2e6506
TheNightmareX <TheNightmareX@foxmail.com>
2021-05-16 11:33:44 +0800

    feat(dtos): optimize structure

--- src/dtos/query-dto.factory.ts
+++ src/dtos/query-dto.factory.ts
@@ -11,61 +11,21 @@ export class QueryDtoFactory<Entity> extends AbstractFactory<QueryDto<Entity>> {
 
   constructor(options: QueryDtoFactoryOptions<Entity>) {
     super();
-
     this.options = this.standardizeOptions(options);
-
-    type Interface = QueryDto<Entity>;
-    this.product = class QueryDto implements Interface {
-      limit? = options.limit?.default;
-      offset? = options.offset?.default;
-      expand? = options.expand?.default;
-      order? = options.order?.default;
-    };
-
-    this.defineTypeMetadata("limit", Number)
-      .applyPropertyDecorators(
-        "limit",
-        Type(() => Number),
-        IsOptional(),
-        IsNumber(),
-        Min(1),
-        ...(options.limit?.max ? [Max(options.limit.max)] : [])
-      )
-
-      .defineTypeMetadata("offset", Number)
-      .applyPropertyDecorators(
-        "offset",
-        Type(() => Number),
-        IsOptional(),
-        IsNumber(),
-        Min(1),
-        ...(options.offset?.max ? [Max(options.offset.max)] : [])
-      )
-
-      .defineTypeMetadata("expand", Array)
-      .applyPropertyDecorators(
-        "expand",
-        Type(() => String),
-        IsOptional(),
-        IsArray(),
-        IsIn(options.expand?.in ?? [], { each: true })
-      )
-
-      .defineTypeMetadata("order", Array)
-      .applyPropertyDecorators(
-        "order",
-        Type(() => String),
-        IsOptional(),
-        IsArray(),
-        IsIn(options.order?.in ?? [], { each: true })
-      );
+    this.product = this.createRawClass();
+    this.defineValidations();
   }
 
   protected standardizeOptions(options: QueryDtoFactoryOptions<Entity>) {
-    const { order } = options;
+    const { limit = {}, offset = {}, expand = {}, order = {} } = options;
 
     return {
-      ...options,
+      limit,
+      offset,
+      expand: {
+        ...expand,
+        in: expand.in ?? [],
+      },
       order: {
         ...order,
         in: [
@@ -77,5 +37,59 @@ export class QueryDtoFactory<Entity> extends AbstractFactory<QueryDto<Entity>> {
         ] as OrderQueryParam<Entity>[],
       },
     };
+  }
+
+  protected createRawClass() {
+    const { limit, offset, expand, order } = this.options;
+
+    type Interface = QueryDto<Entity>;
+    return class QueryDto implements Interface {
+      limit? = limit.default;
+      offset? = offset.default;
+      expand? = expand.default;
+      order? = order.default;
+    };
+  }
+
+  protected defineValidations() {
+    const { limit, offset, expand, order } = this.options;
+
+    this.defineTypeMetadata("limit", Number)
+      .applyPropertyDecorators(
+        "limit",
+        Type(() => Number),
+        IsOptional(),
+        IsNumber(),
+        Min(1),
+        ...(limit.max ? [Max(limit.max)] : [])
+      )
+
+      .defineTypeMetadata("offset", Number)
+      .applyPropertyDecorators(
+        "offset",
+        Type(() => Number),
+        IsOptional(),
+        IsNumber(),
+        Min(1),
+        ...(offset.max ? [Max(offset.max)] : [])
+      )
+
+      .defineTypeMetadata("expand", Array)
+      .applyPropertyDecorators(
+        "expand",
+        Type(() => String),
+        IsOptional(),
+        IsArray(),
+        IsIn(expand.in, { each: true })
+      )
+
+      .defineTypeMetadata("order", Array)
+      .applyPropertyDecorators(
+        "order",
+        Type(() => String),
+        IsOptional(),
+        IsArray(),
+        IsIn(order.in, { each: true })
+      );
   }
 }

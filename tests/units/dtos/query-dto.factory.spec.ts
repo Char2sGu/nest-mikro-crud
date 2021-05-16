693052170f663d50697411459e6cecf2407a18c3
TheNightmareX <TheNightmareX@foxmail.com>
2021-05-16 10:21:18 +0800

    test: complete unit tests for `QueryDtoFactory`

--- tests/units/dtos/query-dto.factory.spec.ts
+++ tests/units/dtos/query-dto.factory.spec.ts
@@ -1,18 +1,59 @@
+import { plainToClass } from "class-transformer";
+import { validate, validateOrReject } from "class-validator";
 import { QueryDtoFactory } from "src";
 import { ParentEntity } from "tests/entities";
+import { buildKeyChecker } from "tests/utils";
 
 describe(QueryDtoFactory.name, () => {
+  const d = buildKeyChecker<typeof factory>();
+
   let factory: QueryDtoFactory<ParentEntity>;
 
-  describe("()", () => {
-    beforeEach(() => {
-      factory = new QueryDtoFactory({
-        order: { in: ["child1", "child1:asc"] },
-      });
+  beforeEach(() => {
+    factory = new QueryDtoFactory<ParentEntity>({
+      limit: { max: 2, default: 1 },
+      offset: { max: 4, default: 3 },
+      expand: { in: ["child1"], default: ["child2"] },
+      order: { in: ["child1", "child1:asc"], default: ["id:desc"] },
+    });
+  });
+
+  it("should standardize the order options", () => {
+    expect(factory.options.order.in).toEqual(["child1:asc", "child1:desc"]);
+  });
+
+  describe(d(".product"), () => {
+    const d = buildKeyChecker<typeof factory.product.prototype>();
+
+    let instance: typeof factory.product.prototype;
+
+    it("should assign default values when optional properties are not provided", () => {
+      instance = plainToClass(factory.product, {});
+      expect(instance.limit).toBe(1);
+      expect(instance.offset).toBe(3);
+      expect(instance.expand).toEqual(["child2"]);
+      expect(instance.order).toEqual(["id:desc"]);
     });
 
-    it("should standardize the order options", () => {
-      expect(factory.options.order.in).toEqual(["child1:asc", "child1:desc"]);
-    });
+    it.each`
+      name        | value
+      ${"limit"}  | ${0}
+      ${"limit"}  | ${3}
+      ${"limit"}  | ${"nan"}
+      ${"offset"} | ${0}
+      ${"offset"} | ${5}
+      ${"offset"} | ${"nan"}
+      ${"expand"} | ${["unknown"]}
+      ${"expand"} | ${"notanarray"}
+      ${"order"}  | ${["unknown"]}
+      ${"order"}  | ${"notanarray"}
+    `(
+      "should throw an error when `$name` is $value",
+      async ({ name, value }) => {
+        await expect(
+          validateOrReject(plainToClass(factory.product, { [name]: value }))
+        ).rejects.toBeDefined();
+      }
+    );
   });
 });

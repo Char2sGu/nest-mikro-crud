d63d6ad5ef80c7a3ba5ac4e75e8b12dfa95ca1f3
TheNightmareX <TheNightmareX@foxmail.com>
2021-05-21 20:15:46 +0800

    fix: make arrays of entities relation fields

--- src/types/relation-path.type.ts
+++ src/types/relation-path.type.ts
@@ -2,5 +2,5 @@ import { ExtractNestedKeys } from "../utils";
 
 export type RelationPath<Entity> = ExtractNestedKeys<
   Entity,
-  Record<string, any>
+  Record<string, any> | Record<string, any>[]
 >;

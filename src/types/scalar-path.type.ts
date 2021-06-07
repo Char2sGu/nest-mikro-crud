474bad3be1c55889423898d5f9c09c915a38e93d
TheNightmareX <TheNightmareX@foxmail.com>
2021-06-07 22:54:42 +0800

    style: update comment

--- src/types/scalar-path.type.ts
+++ src/types/scalar-path.type.ts
@@ -3,5 +3,9 @@ import { ExtractPath } from "src/utils";
 
 /**
  * _`String.length` is not excluded because excluding it will cause performance problems_
+ *
+ * To exclude length
+ * @example
+ * Exclude<ExtractPath<Entity, Scalar>, `${string}.length`>;
  */
 export type ScalarPath<Entity> = ExtractPath<Entity, Scalar>;

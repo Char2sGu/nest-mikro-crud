05fe1db840ac7eefd18fac677b5f85f12dbdaf6b
TheNightmareX <TheNightmareX@foxmail.com>
2021-06-07 14:48:11 +0800

    style: add comment

--- src/types/scalar-path.type.ts
+++ src/types/scalar-path.type.ts
@@ -1,4 +1,7 @@
 import { Scalar } from "@mikro-orm/core/typings";
 import { ExtractPath } from "src/utils";
 
+/**
+ * _`String.length` is not excluded because excluding it will cause performance problems_
+ */
 export type ScalarPath<Entity> = ExtractPath<Entity, Scalar>;

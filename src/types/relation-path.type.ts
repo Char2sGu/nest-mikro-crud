1aacdcf2d89a822de8e69ac36209037dae67feab
TheNightmareX <TheNightmareX@foxmail.com>
2021-06-07 22:50:37 +0800

    fix: exclude `Date` type from relation paths

--- src/types/relation-path.type.ts
+++ src/types/relation-path.type.ts
@@ -1,7 +1,46 @@
 import { ExtractPath } from "../utils";
 
+/**
+ * Try it by yourself?
+ * @example
+ * interface User {
+ *   id: number;
+ *   classroomsCreated: Collection<Classroom>;
+ *   joinApplications: Collection<JoinApplication>;
+ *   memberships: Collection<Membership>;
+ *   createdAt: Date;
+ *   updatedAt: Date;
+ * }
+ *
+ * interface Membership {
+ *   id: number;
+ *   owner: User;
+ *   classroom: Classroom;
+ *   createdAt: Date;
+ *   updatedAt: Date;
+ * }
+ *
+ * interface Classroom {
+ *   id: number;
+ *   creator: User;
+ *   joinApplications: Collection<JoinApplication>;
+ *   memberships: Collection<Membership>;
+ *   createdAt: Date;
+ *   updatedAt: Date;
+ * }
+ *
+ * interface JoinApplication {
+ *   id: number;
+ *   owner: User;
+ *   classroom: Classroom;
+ *   createdAt: Date;
+ *   updatedAt: Date;
+ * }
+ *
+ * type T = RelationPath<Membership>;
+ */
 export type RelationPath<Entity> = ExtractPath<
   Entity,
-  null | Record<string, any> | Record<string, any>[],
-  Function
+  null | Record<string, any>,
+  Function | Date
 >;
